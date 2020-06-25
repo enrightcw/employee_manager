75dddadb098b3f9407522d9ba6975db0c8680d55
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-06-24 23:04:08 -0400

    First pass on all of the functions, still need to get update employees working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer");
+var inquirer = require('inquirer');
+const { response } = require("express");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,7 +17,211 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  start();
-});
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n"); 
+    start();
+    });
+
+    connection.connect(function (err){
+        console.log("Connected as id: "+ connection.threadID);
+    });
+
+var start = function(){
+        inquirer.prompt({
+        type: "list", 
+        choices: [
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "View Department",
+            "View Roles", 
+            "View Employees",
+            "Update Employee Role", 
+            "Quit"
+        ],
+        message: "What would you like to do?",
+        name: "option"
+        })
+        .then(function(result){
+            console.log("You have entered: " + result.option);
+
+            switch (result.option){
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "View Department":
+                    viewDepartment();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+                case "View Employees":
+                    viewEmployees();
+                    break;
+                case "Update Employee Role":
+                    updateEmployee();
+                    break;
+                    default:
+                    quit();
+                    
+            }
+        });
+        
+    };
+
+
+
+
+function addDepartment(){
+
+    inquirer.prompt({
+
+        type: "input",
+        message: "What is the name of the department?",
+        name: "deptName"
+    }).then(function(answer){
+
+        //this still needs works
+        connection.query("Insert into Department (name) VALUES (?) "), [answer.deptName], function (err,res){
+            if (err) throw (err);
+            console.table(res)
+            start();
+            
+        }
+    })
+}
+// start();
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "List",
+            message: "Please select your role?", 
+            choices: [
+                "Engineer",
+                "Finance",
+                "Legal",
+                "Sales"
+            ],
+            name: "addRoleTitle"
+        },
+        {
+            type: "input",
+            message: "What is the salary for this role?",
+            name: "salaryTot"
+        },
+        {
+            type: "input",
+            message: "Please list the department ID Number.",
+            name: "deptID"
+        }
+    ]).then(function(answer){
+        connection.query("insert into role (x,y,z) values ???"), [answer.addRoleTitle, answer.salaryTot, answer.deptID], function( err, res){
+            if (err) throw (err);
+            console.table(answer);
+            start();
+        }
+    })
+};
+
+function updateEmployee(){
+  let query ="SELECT * FROM employee";
+    connection.query(query, function(err,res){
+        if (err) throw err;
+        console.table(res);
+  
+  inquirer.prompt([
+    {
+        type: "list",
+        name: "empChoice",
+        message: "Which employee would you like to update?",
+        choices: function(value){
+          var choicearray = [];
+          for(var i=0; i < res.length; i++){
+            choicearray.push(res[i].itemname);
+          }
+          return choicearray;
+        },
+    }
+    
+    ]).then(function(result){
+      console.log("You have entered: " + result.empChoice);
+
+      switch (result.empChoice){
+          case "Add Department":
+              addDepartment();
+              break;
+          case "Add Role":
+              addRole();
+              break;
+          case "Add Employee":
+              addEmployee();
+              break;
+          case "View Department":
+              viewDepartment();
+              break;
+          case "View Roles":
+              viewRoles();
+              break;
+          case "View Employees":
+              viewEmployees();
+              break;
+          case "Update Employee Role":
+              updateEmployee();
+              break;
+              default:
+              quit();
+              
+      }
+})
+}
+
+function viewDepartment() {
+    let query ="SELECT * FROM department";
+    connection.query(query, function(err,res){
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+function viewEmployees() {
+    let query ="SELECT * FROM employee";
+    connection.query(query, function(err,res){
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+function viewRoles() {
+    let query ="SELECT * FROM role";
+    connection.query(query, function(err,res){
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+function quit(){
+    connection.end();
+    process.exit();
+}
+//Variables Here, class and constructors: department, employee, manager
+
+
+//inquirer start "What do you want to do?"
+
+//inquirer CRUD
+
+//DB CRUD
+
+//seperate files for employee.js, manager.js, and DB CRUD.js
+
+//seed.sql
